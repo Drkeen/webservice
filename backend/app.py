0dcfac26aebb92f8232f6d298d7465ae56f1dac2
Drkeen <babovicsien@gmail.com>
2025-04-21 15:05:56 +1000

    Added Ping route

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"])
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route('/ping')
+def ping():
+    return jsonify({"message": "pong-dev", "status": "ok"}), 200
 
 
 if __name__ == '__main__':
